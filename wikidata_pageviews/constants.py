c0c8e1f72e4e8a0fe7371c69be2e46e4e0e0c825
Gavin Matthews <gmatthews@ntent.com>
2018-10-16 16:53:17 -0700

    Add constants file

---
+++ wikidata_pageviews/constants.py
@@ -0,0 +1,8 @@
+"""Some convenient constants.
+"""
+from pathlib import Path
+
+DEFAULT_DATABASE = 's53865__wdpv_p'
+DEFAULT_DIR = '/public/dumps/pageviews'
+DEFAULT_OUTPUT = Path.home() / 'www' / 'static' / 'latest.json'
+DEFAULT_DURATIONS = ['1d', '1w']
